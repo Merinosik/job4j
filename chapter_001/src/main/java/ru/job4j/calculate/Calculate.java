806b1b6eeb0db507b4b132d614c920a47e56f7fc
Andrey Tatuev <andrejtatuev@gmail.com>
2019-05-25 00:10:54 +0300

    1.3.1 Добавление плагина для генерации JavaDoc [#131]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,20 @@
 package ru.job4j.calculate;
+
+/**
+ * Вывод строки в консоль.
+ *
+ * @author Andrey Tatuev (andrejtatuev@gmail.com)
+ * @version $Id$
+ * @since 5/24/2019
+ */
 public class Calculate{
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
+
 		System.out.println("Hello World");
+
 	}
 }
